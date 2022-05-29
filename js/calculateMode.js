10e9d05c886efda182146b74ddccf0e21c72c402
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-28 22:10:54 -0700

    finished js

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,32 @@
-const calculateMode = () => {
+exports.calculateMode = (originalList) => {
 
+    let newObject = {};
+    let finalMode = [];
+
+    // Creates an object
+    for (let i = 0; i < originalList.length; i++){
+       if (newObject.hasOwnProperty(originalList[i])){
+           newObject[originalList[i]]++
+       }else{
+        newObject[originalList[i]] = 1
+       }
+    }
+    let finalValues = Object.values(newObject);
+    // console.log(newObject);
+
+    // Checks if the props value is also the highest value in the array
+    for (let prop in newObject){
+        if (newObject[prop] == Math.max.apply(0, finalValues)){
+            finalMode.push(prop);
+        } 
+    }
+    for (let k = 0; k < finalMode.length; k++){
+        if (isNaN(finalMode[k]) == true) continue;
+        finalMode[k] = parseInt(finalMode[k]);
+    }
+    console.log(finalMode);
 }
+// calculateMode([1,3,4,4,2,1]);
+// calculateMode([2,2,1,7,10,12,13]);
+// calculateMode([1,10,4,5,3,15,1,8,7,15]);
+// calculateMode(["Who", "Hello", "Who"]);
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,8 @@
-// Write your own unit tests here!
+var sp = require("./calculateMode");
+
+
+
+sp.calculateMode([1,3,4,4,2,1]);
+sp.calculateMode([2,2,1,7,10,12,13]);
+sp.calculateMode([1,10,4,5,3,15,1,8,7,15]);
+sp.calculateMode(["Who", "Hello", "Who"]);
